e2dbd057bc57f1d7fb81e132adb09d2dfcff61a0
Yang-jaemin <108872973+Yang-jaemin@users.noreply.github.com>
2022-09-27 23:32:45 +0900

    algorithm study

--- practice.py
+++ practice.py
@@ -1,2 +1,15 @@
+def solution(s):
+    a = []
+    for i in s:
+        a.append(i)
+        
+    if a[0]=='+':
+        answer = int("".join(a[1:]))
+    elif a[0]=='-':
+        answer = -(int("".join(a[1:])))
+    else: 
+        answer = int("".join(a))
+        
+    return answer
 
-    
+print(solution('12345133123'))
---
+++ 메모/programmers lv1.py
@@ -0,0 +1,41 @@
+##### 9월 27일
+# 문자열을 정수로 바꾸기
+# 정수 내림차순으로 배치하기
+# 정수 제곱근 판별
+# 문자열 P와 Y의 갯수
+
+
+# 리스트의 원소 개수
+len(list)
+
+# 리스트의 원소의 합
+sum(list)
+
+# 루트 표현
+a**(1/2)
+
+# 리스트에 추가
+list.append()
+
+# 리스트 역순
+list.reverse()
+
+# 문자열 집계함수 count
+s = 'asdaas'
+s.count('a') # 3
+
+# join 함수
+answer = "".join(a) # 문자열 리스트를 붙여서 걍 문자열로 만듬 int() 하면 정수로도 바꿀 수 있을걸요
+
+# 숫자끼리 떼어서 리스트로 정렬
+while n > 0:
+        r = n % 10
+        n = n // 10
+        lst.append(r)
+        
+# string에서 - +를 부호로 인식 : 문자열을 정수로 바꾸기
+def strToInt(str):
+    
+    a = int(str)
+    return a
+
